0f8c9b5cc856533cde4df028d7c1a483361a3ac1
Martyna Zukowska <martynazukowska7@interia.pl>
2021-04-22 08:42:24 +0200

    poprawione sortowanie w %

--- sortowanie/src/sorting_performance.cpp
+++ sortowanie/src/sorting_performance.cpp
@@ -5,13 +5,13 @@
 #include <iostream>
 
 #define ZAKRES       50
-#define ILOSC      /* 10000 */10000 /*1000000s*/
+#define ILOSC      /* 10000 */100/*1000000s*/
 
 using namespace std::chrono_literals;
 
 void wypelnij(std::vector<int> &tab,int zmienna)
 {
-    int x;
+    int x=0;
     for (int j = 0; j < zmienna; j++)
     {
         x = rand() % ZAKRES;
@@ -28,12 +28,23 @@ void wypelnij1(std::vector<int> &tab,int zmienna)
 
 void Sortowanie (std::vector<int> &tab, std::vector<int> &tab2, float x)
 {
-    for(int j=(tab.size()*x);j<tab.size();j++)
+
+    int znienna=tab.size();
+    std::cout<<"\n";
+    for(int j=znienna-(znienna*x);j<znienna;j++)
     {
         tab[j]=tab2[j];
     } 
+    std::cout<<"\n";
+    for(int i=0;i<znienna;i++)
+    {
+        std::cout<<tab[i]<<"\t";
+    }
+
 }
-const float procent_sortowania[6]={0.25,0.5,0.75,0.95,0.99,0.997};
+const float procent_sortowania[4]={0.25,0.5,0.75,0.95};
+
+
 
 
 
@@ -51,7 +62,6 @@ int main(int argc, char* argv[])
 
 
     wypelnij(tab,zmienna);
-
     pom=tab;
 
     Timer timer;
@@ -62,26 +72,30 @@ int main(int argc, char* argv[])
     timer.stop();
     std::cout << "Waited quickSort for " << timer.sInterval() << "s\n";
 
-     for(int j=0;j<6;j++)    //petla odpowiadajaca za zmienianie i sortowanie tablicy "posortowanej"
+
+
+    
+     for(int j=0;j<4;j++)    //petla odpowiadajaca za zmienianie i sortowanie tablicy "posortowanej"
     {
-        Timer timer1;
+    
         posortowana=pom;
+        Timer timer1;
         Sortowanie(posortowana,tab,procent_sortowania[j]);
         std::cout<<"Sortowanie przez Quick vectora posortowanego w " << procent_sortowania[j]*100 << "%...\n";
-        timer1.precision();
         timer1.start();
-        insertSort(posortowana);
+        quickSort(posortowana,0,zmienna-1);
         std::this_thread::sleep_for(3s);
         timer1.stop();
         std::cout << "Waited for " << timer1.sInterval()<< czas << "s\n";
     }
 
+    tab=pom;
+
         std::cout<<"\n";
-        
+
 
     
     wypelnij1(tab,zmienna);
-    pom=tab;
 
     Timer timer2;
     timer2.start();
@@ -93,23 +107,48 @@ int main(int argc, char* argv[])
     std::cout<<"\n";
     std::cout<<"\n";
 
-    wypelnij(tab,zmienna);
-    pom=tab;
+
+    tab=pom;
+
+    Timer timer4;
+    timer4.start();
+    insertSort(tab);
+    std::this_thread::sleep_for(3s);
+    timer4.stop();
+    std::cout << "Waited insert for " << timer4.sInterval() << "s\n";
+
+     for(int j=0;j<4;j++)    //petla odpowiadajaca za zmienianie i sortowanie tablicy "posortowanej"
+    {
+
+        posortowana=pom;
+        Timer timer1;
+        Sortowanie(posortowana,tab,procent_sortowania[j]);
+        std::cout<<"Sortowanie przez Insert vectora posortowanego w " << procent_sortowania[j]*100 << "%...\n";
+        timer1.start();
+        insertSort(posortowana);
+        std::this_thread::sleep_for(3s);
+        timer1.stop();
+        std::cout << "Waited  for " << timer1.sInterval()<< czas << "s\n";
+    }
+
+    std::cout<<"\n";
+
+    tab=pom;
 
     Timer timer3;
     timer3.start();
     mergeSort(tab,0,zmienna-1);
     std::this_thread::sleep_for(3s);
     timer3.stop();
-    std::cout << "Waited przez scalanie for " << timer3.sInterval() << "s\n";
 
-     for(int j=0;j<6;j++)    //petla odpowiadajaca za zmienianie i sortowanie tablicy "posortowanej"
+    std::cout << "Waited przez scalanie for " << timer3.sInterval() << "s\n";
+     for(int j=0;j<4;j++)    //petla odpowiadajaca za zmienianie i sortowanie tablicy "posortowanej"
     {
-        Timer timer1;
+
         posortowana=pom;
+        Timer timer1;
         Sortowanie(posortowana,tab,procent_sortowania[j]);
         std::cout<<"Sortowanie przez Merge vectora posortowanego w " << procent_sortowania[j]*100 << "%...\n";
-        timer1.precision();
         timer1.start();
         mergeSort(posortowana,0,zmienna-1);
         std::this_thread::sleep_for(3s);
@@ -119,29 +158,6 @@ int main(int argc, char* argv[])
     std::cout<<"\n";
     std::cout<<"\n";
 
-    wypelnij(tab,zmienna);
-    pom=tab;
-
-    Timer timer4;
-    timer4.start();
-    insertSort(tab);
-    std::this_thread::sleep_for(3s);
-    timer4.stop();
-    std::cout << "Waited insert for " << timer4.sInterval() << "s\n";
-
-     for(int j=0;j<6;j++)    //petla odpowiadajaca za zmienianie i sortowanie tablicy "posortowanej"
-    {
-        Timer timer1;
-        posortowana=pom;
-        Sortowanie(posortowana,tab,procent_sortowania[j]);
-        std::cout<<"Sortowanie przez Insert vectora posortowanego w " << procent_sortowania[j]*100 << "%...\n";
-        timer1.precision();
-        timer1.start();
-        insertSort(posortowana);
-        std::this_thread::sleep_for(3s);
-        timer1.stop();
-        std::cout << "Waited  for " << timer1.sInterval()<< czas << "s\n";
-    }
-
+    
     return 0;
 }
